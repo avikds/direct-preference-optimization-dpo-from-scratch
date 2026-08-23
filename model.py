4ad3c9d787acef54d6a3e9a7ed18a04f64ae431d
Avik Das <avikd957@gmail.com>
2026-08-23 11:08:39 +0530

    Step 11: build_preference_pairs

--- model.py
+++ model.py
@@ -137,8 +137,18 @@ def reward_accuracy(reward_chosen, reward_rejected):
 
     return np.mean(reward_chosen > reward_rejected)
 
-# Step 11 - build_preference_pairs (not yet solved)
-# TODO: implement
+# Step 11 - build_preference_pairs
+def build_preference_pairs(prompts, chosen_ids, rejected_ids, chosen_mask, rejected_mask):
+    return [
+        {
+            "prompt": prompts[i],
+            "chosen_ids": chosen_ids[i],
+            "rejected_ids": rejected_ids[i],
+            "chosen_mask": chosen_mask[i],
+            "rejected_mask": rejected_mask[i],
+        }
+        for i in range(len(prompts))
+    ]
 
 # Step 12 - sample_preference_batch (not yet solved)
 # TODO: implement
