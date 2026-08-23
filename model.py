bf149c2ec419019d9e3aea7cf500b01f4f6e3e42
Avik Das <avikd957@gmail.com>
2026-08-23 11:04:59 +0530

    Step 7: policy_sequence_logprob

--- model.py
+++ model.py
@@ -78,8 +78,12 @@ def policy_token_logits(params, token_ids):
     hidden = embed[token_ids]
     return hidden @ W_out + b_out
 
-# Step 7 - policy_sequence_logprob (not yet solved)
-# TODO: implement
+# Step 7 - policy_sequence_logprob
+def policy_sequence_logprob(params, token_ids, mask):
+    logits = policy_token_logits(params, token_ids)
+    log_probs = log_softmax(logits, axis=-1)
+    token_logprobs = gather_token_logprobs(log_probs, token_ids)
+    return masked_sequence_logprob(token_logprobs, mask)
 
 # Step 8 - sequence_logprob_grad (not yet solved)
 # TODO: implement
